413880d6fa00f3a69dd137cdf2a908aa06d8c130
H20Winter <32801316+H20Winter@users.noreply.github.com>
2017-10-14 14:58:31 -0400

    Update untitled-1.py

--- untitled-1.py
+++ untitled-1.py
@@ -5,10 +5,10 @@ guess = int(input("input a number 0 to 1000: "))
 if guess < 0 and guess > 1000:
     print("that isn't on the list")
     
-if guess is answer:
+elif guess is answer:
     print ("yee your right")
 
-if guess is 42:
+elif guess is 42:
     print("it's the answer to most things but not this")
     
 else:
