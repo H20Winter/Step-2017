8da6da97bb5b9e3037af4e7eb189242afc32824f
H20Winter <32801316+H20Winter@users.noreply.github.com>
2017-10-14 14:54:51 -0400

    Add files via upload

---
+++ If statments.py
@@ -0,0 +1,14 @@
+age = int(input("Enter your age => "))
+
+if age > 20 and age < 30:
+    print ("you are in your 20's ")
+     
+if age > 80:
+    print ("ur old")
+
+if age < 3:                    
+    print(" baby ")
+
+if age < 20 and age > 3:
+    print("You are a teen or a kid")
+
